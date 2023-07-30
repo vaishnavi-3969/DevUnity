47e2df5041898e24807ecc5a03afbfdd9e54c213
Pratyaksh Aggarwal <pratyaksh2002@gmail.com>
2023-07-30 14:21:03 +0530

    new commit

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
-    <section className="flex">
+    isAuthenticated && (
+      <section className="flex">
         <Sidebar/>
-        <div>
-            <h1 className="color-black px-20 py-10 text-4xl">My Profile</h1>
-            
+        <div className='ml-10 mt-10'>
+            <img src={user.picture} alt={user.name} className='rounded-full'/>
+            <h2 className='font-bold'>{user.name}</h2>
+            <p className='font-sans'>Email: {user.email}</p>
         </div>
-    </section>
-  )
-}
+
+
+        <div class="listing-container">
+          <form class="inputs-container">
+            
+            <input type="text" name="cpt-title" class="simple-input" placeholder="Nom de la sauce" autocomplete="none" />
+            
+            
+            <span data-label="Qu'avez-vous mélangé comme sauce ?">
+              <input type="text" placeholder="Ajouter une sauce" class="js-sauce-name" />
+            </span>
+
+            <span class="js-add-sauce">
+              <a href="#addSauce"><i class="icon icon-add"></i></a>
+            </span>
+          </form>
+        </div>
+      </section>
+    )  
+  );
+};
 
 export default Profile
--- src/components/Profile/ProfileCard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProfileCard = () => {
-  return (
-    <div>ProfileCard</div>
-  )
-}
-
-export default ProfileCard
